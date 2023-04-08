c14b3a1b87bef79f4321db728b5036651c12a615
Dzulfiqar Fathur Rahman <contact@dzulfiqarfr.com>
2023-04-08 12:39:00 +0700

    Add `parse_period_id_to_month()` as private helper
    Add the function to parse period IDs into month numbers as
    a private helper. This is the more appropriate place to define
    it, as opposed to the script to build and update the internal
    data as it used to.
    
    `bps_add_date()` now throws an error when there is no period
    table in the internal data. This gives a more helpful context
    to users.

--- R/date.R
+++ R/date.R
@@ -30,9 +30,13 @@ bps_add_date <- function(data) {
   }
 
   if (has_period_id) {
-    return(make_date_with_period_id(data))
+    # Add date using `year` and `period_id` for datasets with those columns
+    data_with_date <- make_date_with_period_id(data)
+
+    return(data_with_date)
   }
 
+  # Add date using `year` and `month` for trade datasets
   if (!is_integer(data$year) && !is_integer(data$month)) {
     cli_abort("{.var year} and {.var month} must be integers.")
   }
@@ -47,19 +51,20 @@ bps_add_date <- function(data) {
 # Helper ------------------------------------------------------------------
 
 make_date_with_period_id <- function(data, call = caller_env()) {
-  if (!exists("db_period_month")) {
-    cli_alert_warning(
+  if (!exists("period_table")) {
+    cli_abort(
       str_c(
-        "Can't find {.var db_period_month}. ",
+        "Can't find the period table in database. ",
         "Unable to make date out of {.var period_id} and {.var year}."
-      )
+      ),
+      call = call
     )
-
-    return(data)
   }
 
+  period_as_month <- parse_period_id_to_month(period_table)
+
   data |>
-    left_join(db_period_month, by = "period_id") |>
+    left_join(period_as_month, by = "period_id") |>
     dplyr::mutate(
       date = dplyr::case_when(
         !str_detect(period, "Tahun|Annual") ~ lubridate::make_date(year, month)
@@ -67,4 +72,62 @@ make_date_with_period_id <- function(data, call = caller_env()) {
     ) |>
     dplyr::select(!month) |>
     dplyr::relocate(date, .after = period)
+}
+
+
+# Parse the month number of the periods, which can facilitate date creation
+# using period IDs. This is useful for periods such as quarters and semesters
+parse_period_id_to_month <- function(data, call = caller_env()) {
+  required_column <- c("period_id", "period", "period_group")
+
+  if (!all(required_column %in% names(data))) {
+    cli_abort(
+      "{.arg data} must contain {.var {required_column}} columns.",
+      call = call
+    )
+  }
+
+  monthly <- data |>
+    dplyr::filter(period_id %in% seq(1, 12)) |>
+    dplyr::mutate(month = as.integer(period_id))
+
+  quarterly <- data |>
+    dplyr::filter(
+      str_detect(period, "Triwulan") | str_detect(period_group, "Triwulan"),
+      !str_detect(period, "Tahun"),
+      period != "Jumlah"
+    ) |>
+    dplyr::mutate(
+      month = dplyr::case_when(
+        str_detect(period, "Triwulan IV|4") ~ 10,
+        str_detect(period, "Triwulan I{3}|3") ~ 7,
+        str_detect(period, "Triwulan I{2}|2") ~ 4,
+        str_detect(period, "Triwulan I{1}|1") ~ 1,
+        period == "Maret" ~ 1,
+        period == "Juni" ~ 4,
+        period == "September" ~ 7,
+        period == "Desember" ~ 10
+      )
+    )
+
+  semester <- data |>
+    dplyr::filter(
+      str_detect(period_group, "Semester"),
+      !str_detect(period, "Tahun")
+    ) |>
+    dplyr::mutate(
+      period = str_remove(period, "Semester\\s\\d\\s"),
+      period = str_remove_all(period, "[:punct:]")
+    ) |>
+    left_join(monthly[, c("period", "month")], by = "period")
+
+  annual <- data |>
+    dplyr::filter(str_detect(period, "Tahun") | period == "Jumlah") |>
+    dplyr::mutate(month = NA)
+
+  parsed <- dplyr::bind_rows(monthly, quarterly, semester, annual)
+
+  parsed |>
+    dplyr::mutate(month = as.integer(month)) |>
+    dplyr::select(period_id, month)
 }
